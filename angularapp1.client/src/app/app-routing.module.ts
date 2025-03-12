f12aab4feff099edaace1f18e6d10b804a3a74f8
adnan alghzawi <adnanalghzawi7@gmail.com>
2025-03-12 22:48:25 +0300

    Add project files.

---
+++ angularapp1.client/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductComponent } from './product/product.component';
+
+
+const routes: Routes = [
+  { path: "login", component: SignInComponent },
+  { path: "register", component: SignUpComponent },
+  { path: "category", component: CategoryComponent },
+  { path: "product", component: ProductComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ angularapp1.client/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductComponent } from './product/product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    SignUpComponent,
+    SignInComponent,
+    CategoryComponent,
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule, HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
